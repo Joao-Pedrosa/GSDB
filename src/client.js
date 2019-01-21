92a76254037318c17e7bd30a4913d2000824fcd5
husky <nameisgleb@gmail.com>
2019-01-21 21:30:39 +0100

    add leaflet map

--- src/client.js
+++ src/client.js
@@ -1,2 +1,12 @@
+const map = L.map('map').setView([51.505, -0.09], 13);
 
-console.log('it\'s alive!');
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={token}', {
+  attribution: [
+    'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors,',
+    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,',
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  ].join(' '),
+  maxZoom: 18,
+  id: 'mapbox.streets',
+  token: 'pk.eyJ1IjoiaHVza3ktZyIsImEiOiJjanI0ejUxeXcwcWl2NDN0ZzFhZ3Zha3lsIn0.V--h7EUXKg0OXMIHetCU-g',
+}).addTo(map);
